b259358a30224561cf991b462f9b80faa676ca61
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-04-12 18:51:45 -0300

    add basic test structure

--- Filtro de Faturas/src/Cliente.java
+++ Filtro de Faturas/src/Cliente.java
@@ -3,6 +3,11 @@ import java.util.Date;
 public class Cliente {
     String nome;
     Date dataDeInclusao;
-
     Estado estado;
+
+    public Cliente(String nome, Date dataDeInclusao, Estado estado) {
+        this.nome = nome;
+        this.dataDeInclusao = dataDeInclusao;
+        this.estado = estado;
+    }
 }
