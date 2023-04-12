fee92e232883677b61d5a8125648a90dfe440630
Lucas Albuquerque <lucasbrenner121@gmail.com>
2023-04-12 20:11:12 -0300

    add tests

--- Filtro de Faturas/test/FaturaTest.java
+++ Filtro de Faturas/test/FaturaTest.java
@@ -1,5 +1,32 @@
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class FaturaTest {
+
+    Cliente cliente;
+    Date data;
+    Estado pb;
+
+    @BeforeEach
+    public void setup() {
+        data = new Date();
+        pb = new Estado("PB");
+        cliente = new Cliente("fulano", data, pb);
+    }
+
+    @Test
+    public void testValorNegativo() {
+        try {
+            Fatura fatura = new Fatura("333", -3, data, cliente);
+            fail("Valor não pode ser negativo");
+        } catch (Exception e) {
+        }
+    }
 
 }
