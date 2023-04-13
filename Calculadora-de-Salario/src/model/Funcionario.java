bf9caa6d95365c3adff3a8e8c709b97f0ca24ecb
Leonardo Su <leonardo.su@ccc.ufcg.edu.br>
2023-04-12 21:11:16 -0300

    add test and check invalid parameters values.

--- Calculadora-de-Salario/src/model/Funcionario.java
+++ Calculadora-de-Salario/src/model/Funcionario.java
@@ -1,16 +1,28 @@
 package model;
 
+import javax.management.InvalidAttributeValueException;
+
 public class Funcionario {
     private final String nome;
     private String email;
     private double salario;
     private Cargo cargo;
 
-    public Funcionario(String nome, String email, double salario, Cargo cargo) {
+    public Funcionario(String nome, String email, double salario, Cargo cargo) throws InvalidAttributeValueException {
+        if(salario < 0) {
+            throw new InvalidAttributeValueException("Salario negativo");
+        } else if(nome == null || nome.isEmpty()) {
+            throw new InvalidAttributeValueException("Nome nulo ou vazio");
+        } else if(email == null || email.isEmpty()) {
+            throw new InvalidAttributeValueException("Email nulo ou vazio");
+        }
+
         this.nome = nome;
         this.email = email;
         this.salario = salario;
         this.cargo = cargo;
+
+
     }
 
     public String getNome() {
